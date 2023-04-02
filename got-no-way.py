4490b0ca7a07b8e81e2c5ea2bd45f0fe47156653
Maor M. Kertsman <maorkertsman@gmail.com>
2023-04-02 14:35:13 +0300

    Fix conventions

--- got-no-way.py
+++ got-no-way.py
@@ -5,17 +5,12 @@ def get_path() -> str:
     return input('Please enter path -> ')
 
 
-def get_file_list(path_to_file: str) -> list:
-    directory = None
-    try:
-        directory = os.listdir(path_to_file)
-    except:
-        show_error('Error opening file.')
-    return directory
-
-
-def show_error(text_to_display: str):
-    print(text_to_display)
+def get_file_list(path_to_directory: str) -> list:
+    '''
+    :param path_to_driectory: path to directory as string
+    :return: a list that contains the names of all files inside directory
+    '''
+    return os.listdir(path_to_directory)
 
 
 def show_results(results: list):
@@ -23,20 +18,15 @@ def show_results(results: list):
 
 
 def get_files_starting_with_name(file_list: list, name: str) -> list:
-    result_list = [check_name(file, name) for file in file_list if check_name(file, name) is not None]
-
-    return result_list
+    return [check_name(file, name) for file in file_list if check_name(file, name) is not None]
 
 
-def check_name(file: str, name: str) -> str:
-    return file if file[:len(name)] == name else None
+def check_name(file_name: str, name_to_search: str) -> str:
+    return file_name if file_name[:len(name_to_search)] == name_to_search else None
 
 
 if __name__ == '__main__':
     path = get_path()
-
     files = get_file_list(path)
-
     results = get_files_starting_with_name(files, 'deep')
-
     show_results(results)
